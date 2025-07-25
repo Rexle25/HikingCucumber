dfff42f50bd4cfdb7a12827d60719b1a1c9c2873
gurke <gisbert11@protonmail.com>
2025-07-25 11:18:25 +0200

    I did things

--- achievments.js
+++ achievments.js
@@ -2,11 +2,18 @@ function achievments() {
 
     navigator.geolocation.getCurrentPosition( pos => {
 
-
+                alert(pos.coords.altitude);
                 if (pos.coords.altitude > 500) {
 
                     var hügelgurke = localStorage.getItem("hügelgurke");
                     alert(hügelgurke);
+
+                    if (hügelgurke = null) {
+                        alert("nullgurke");
+                        localStorage.setItem("hügelgurke", "true")
+                    } else {
+                        alert("truegurke");
+                    }
                     
 
                 }
