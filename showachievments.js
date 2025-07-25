1c7754583b6ac30e3a7cd545021331e62fa6ec40
gurke <gisbert11@protonmail.com>
2025-07-25 11:49:01 +0200

    I did things

--- showachievments.js
+++ showachievments.js
@@ -3,5 +3,5 @@ function showAchievments() {
 
     const title = document.createElement("h2");
     title.innerHTML = "SUS"
-    document.body.appendChild(title);s
+    document.body.appendChild(title);
 }
