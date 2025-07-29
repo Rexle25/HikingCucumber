35f10e2817756138439d68813cbdfcc82264e4f0
gurke <gisbert11@protonmail.com>
2025-07-29 10:17:10 +0200

    I did things

--- getRegionData.js
+++ getRegionData.js
@@ -18,6 +18,9 @@ function getRegionData(lat, lon) {
       node["tourism"](${south},${west},${north},${east});
       way["tourism"](${south},${west},${north},${east});
       relation["tourism"](${south},${west},${north},${east});
+      node["amenity"](${south},${west},${north},${east});
+      way["amenity"](${south},${west},${north},${east});
+      relation["amenity"](${south},${west},${north},${east});
     );
     out body;
     `;
